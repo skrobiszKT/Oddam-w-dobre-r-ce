e669de3ae56dd7c21f9be7fb40166cc65add728e
skrobiszKT <skrobisz.kt@gmail.com>
2022-09-01 16:05:38 +0200

    2-dynamiczne-statystyki

--- project/donation/views.py
+++ project/donation/views.py
@@ -1,11 +1,24 @@
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect, get_list_or_404
 from django.views import View
+
+from donation.models import Donation, Institution
+
+
 # Create your views here.
 
 class LandingPageView(View):
     def get(self, request):
-        return render(request, "index.html", {})
+        donations = Donation.objects.all()
+        quantity = 0
+        for donation in donations:
+            quantity = quantity + donation.quantity
+        institutions = Institution.objects.all().count()
+        ctx = {
+            "quantity": quantity,
+            "institutions": institutions
+        }
+        return render(request, "index.html", ctx)
 
 class AddDonationView(View):
     def get(self, request):
