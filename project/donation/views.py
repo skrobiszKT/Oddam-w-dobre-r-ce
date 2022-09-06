a167de04b2aa32315cdfa9c543e404dabdcd7806
skrobiszKT <skrobisz.kt@gmail.com>
2022-09-06 13:22:48 +0200

    feature-2 done

--- project/donation/views.py
+++ project/donation/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect, get_list_or_404
 from django.views import View
 
-from donation.models import Donation, Institution
+from donation.models import Donation, Institution, TYPES
 
 
 # Create your views here.
@@ -14,9 +14,16 @@ class LandingPageView(View):
         for donation in donations:
             quantity = quantity + donation.quantity
         institutions = Institution.objects.all().count()
+        institutions_type_1 = Institution.objects.filter(type=1)
+        institutions_type_2 = Institution.objects.filter(type=2)
+        institutions_type_3 = Institution.objects.filter(type=3)
         ctx = {
             "quantity": quantity,
-            "institutions": institutions
+            "institutions": institutions,
+            "type1": institutions_type_1,
+            "type2": institutions_type_2,
+            "type3": institutions_type_3,
+            "types": TYPES
         }
         return render(request, "index.html", ctx)
 
